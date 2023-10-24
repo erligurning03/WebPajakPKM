e74b0aa9075a8500da3f8d3a44c40e351efb54ee
Nicholas Manurung <agust..nico@gmail.com>
2023-10-24 12:47:55 +0700

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,140 @@
 @extends('layouts.app')
 
+@section('css')
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
+    }
+
+    body {
+        min-height: 100vh;
+        width: 100%;
+        background: #053B50;
+    }
+
+    .containercss {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        max-width: 430px;
+        width: 100%;
+        background: #fff;
+        border-radius: 7px;
+        box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
+    }
+
+    .container .registration {
+        display: none;
+    }
+
+    .container .form {
+        padding: 2rem;
+    }
+
+    .form header {
+        font-size: 2rem;
+        font-weight: 500;
+        text-align: center;
+        margin-bottom: 1.5rem;
+    }
+
+    .form .welcome {
+        font-size: 1.5rem;
+        font-weight: 500;
+        text-align: center;
+        margin-bottom: 1rem;
+        color: #020202e3;
+    }
+
+    .form .logo {
+        text-align: center;
+        margin-top: 1rem;
+    }
+
+    .form .logo img {
+        max-width: 100px;
+        height: auto;
+    }
+
+    .form input {
+        height: 60px;
+        width: 100%;
+        padding: 0 15px;
+        font-size: 17px;
+        margin-bottom: 1.3rem;
+        border: 1px solid #ddd;
+        border-radius: 6px;
+        outline: none;
+    }
+
+    .form input:focus {
+        box-shadow: 0 1px 0 rgba(0, 0, 0, 0.2);
+    }
+
+    .form a {
+        font-size: 16px;
+        color: #009579;
+        text-decoration: none;
+    }
+
+    .form a:hover {
+        text-decoration: underline;
+    }
+
+    .form input.button {
+        color: #fff;
+        background: #E9B824;
+        font-size: 1.2rem;
+        font-weight: 500;
+        letter-spacing: 1px;
+        margin-top: 1.7rem;
+        cursor: pointer;
+        transition: 0.4s;
+    }
+
+    .form input.button:hover {
+        background: #006653;
+    }
+
+    .signup {
+        font-size: 17px;
+        text-align: center;
+    }
+
+    .signup label {
+        color: #E9B824;
+        cursor: pointer;
+    }
+
+    .signup label:hover {
+        text-decoration: underline;
+    }
+</style>
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<div class="containercss">
+    <div class="registration form">
+        <header>Daftar</header>
+        <div class="logo">
+            <img src="{{ asset('img/logoRujak5.jpeg') }}" alt="Logo Rujak">
+        </div>
+        <form action="#">
+            <input type="text" placeholder="Input Email">
+            <input type="password" placeholder="Buat Password">
+            <input type="password" placeholder="Konfirmasi Password">
+            <input type="button" class="button" value="Daftar">
+        </form>
+        <div class="signup">
+            <span class="signup">Sudah Punya Akun?
+                <label for="check">Masuk</label>
+            </span>
         </div>
     </div>
 </div>

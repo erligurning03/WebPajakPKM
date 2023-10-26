70d08f8aa27ef6bbccaedfdf8447048b62c4dc80
erligurning03 <erligurning03@gmail.com>
2023-10-26 14:29:56 +0700

    erli -- front end beranda
    front end tontonan, list tontonan, berita, list berita

--- routes/web.php
+++ routes/web.php
@@ -29,14 +29,21 @@ Route::get('/test', function () {
     return view('loginhomepage');
 });
 
-Route::get('/navbar', function () {
+// Route::get('/navbar', function () {
 
-    return view('navbar/navbar');
-});
+//     return view('navbar/navbar');
+// });
+
+//ini route beranda
 
 Route::get('/beranda', function () {
 
     return view('beranda/beranda');
+});
+
+Route::get('/list_tontonan', function () {
+
+    return view('beranda/list_tontonan');
 });
 
 Route::get('/podcast', function () {
@@ -48,24 +55,42 @@ Route::get('/tontonan', function () {
     return view('beranda/tontonan');
 });
 
+Route::get('/berita', function () {
+
+    return view('beranda/berita');
+});
+Route::get('/list_berita', function () {
+
+    return view('beranda/list_berita');
+});
+
+//ini route semua kuis
+
 Route::get('/kuis', function () {
 
     return view('kuis/kuis');
 });
 
+//ini route semua QNA
 Route::get('/qna', function () {
 
     return view('qna/qna');
 });
+
+//ini route halaman layanan
 Route::get('/layanan', function () {
 
     return view('layanan/layanan');
 });
 
+//ini route profile
+
 Route::get('/profil', function () {
 
     return view('profil/profil');
 });
+
+//ini route semua halaman admin
 Route::get('/admin', function () {
 
     return view('admin/layouts/navbar_admin');
@@ -75,6 +100,10 @@ Route::get('/admin/tontonan', function () {
 
     return view('admin/beranda/tambah_tontonan');
 });
+
+
+
+
 
 
 Auth::routes();

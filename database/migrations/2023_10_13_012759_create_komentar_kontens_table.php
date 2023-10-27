feba8959da4826ce01e441d9b944c06e082d16f3
PaulCak28 <paulussigalingging2803@gmail.com>
2023-10-28 00:12:21 +0700

    Update baru 12:12

--- database/migrations/2023_10_13_012759_create_komentar_kontens_table.php
+++ database/migrations/2023_10_13_012759_create_komentar_kontens_table.php
@@ -13,11 +13,6 @@ return new class extends Migration
     {
         Schema::create('komentar_kontens', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('id_konten'); // Kolom user_id, tipe data unsigned bigint
-            $table->foreign('id_konten')->references('id')->on('kontens');
-            $table->unsignedBigInteger('user_id'); // Kolom user_id, tipe data unsigned bigint
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->string('isi_komentar');
             $table->unsignedBigInteger('konten_id'); // Kolom user_id, tipe data unsigned bigint
             $table->foreign('konten_id')->references('id')->on('kontens');
             $table->unsignedBigInteger('dikomentari_oleh'); // Kolom user_id, tipe data unsigned bigint

e075e8d86455c450c2774bd0a36be29a6156dc60
Nicholas Manurung <agust..nico@gmail.com>
2023-11-11 14:16:45 +0700

    update tampilan konten

--- app/Http/Controllers/Konten_controller.php
+++ app/Http/Controllers/Konten_controller.php
@@ -26,78 +26,78 @@ class Konten_controller extends Controller
         return view('admin/beranda/list_konten', compact('listKonten'));
     }
     //berita
-    // public function indexB()
-    // {
-    //     $listKonten = Konten::with('KomentarKonten')
-    //         ->where('tipe_konten_id', '1')
-    //         ->with('LikeKonten')
-    //         ->with('ShareKonten')
-    //         ->orderBy('created_at', 'DESC')
-    //         ->get();
-    //     // dd($listKonten->toArray());
-    //     return view('beranda.list_berita', compact('listKonten'));
-    // }
-    // public function showB($id)
-    // {
-    //     $listKonten = Konten::with('KomentarKonten')
-    //         ->where('tipe_konten_id', '1')
-    //         ->where('id', $id)
-    //         ->with('LikeKonten')
-    //         ->with('ShareKonten')
-    //         ->orderBy('created_at', 'DESC')
-    //         ->get();
-    //     // dd($listKonten->toArray());
-    //     return view('beranda.berita', compact('listKonten', 'id'));
-    // }
+    public function indexBerita()
+    {
+        $listKonten = Konten::with('KomentarKonten')
+            ->where('tipe_konten_id', '1')
+            ->with('LikeKonten')
+            ->with('ShareKonten')
+            ->orderBy('created_at', 'DESC')
+            ->get();
+        // dd($listKonten->toArray());
+        return view('beranda.list_berita', compact('listKonten'));
+    }
+    public function showBerita($id)
+    {
+        $listKonten = Konten::with('KomentarKonten')
+            ->where('tipe_konten_id', '1')
+            ->where('id', $id)
+            ->with('LikeKonten')
+            ->with('ShareKonten')
+            ->orderBy('created_at', 'DESC')
+            ->get();
+        // dd($listKonten->toArray());
+        return view('beranda.berita', compact('listKonten', 'id'));
+    }
 
     // //Podcast
-    // public function indexP()
-    // {
-    //     $listKonten = Konten::with('KomentarKonten')
-    //         ->where('tipe_konten_id', '2')
-    //         ->with('LikeKonten')
-    //         ->with('ShareKonten')
-    //         ->orderBy('created_at', 'DESC')
-    //         ->get();
-    //     // dd($listKonten->toArray());
-    //     return view('beranda.list_podcast', compact('listKonten'));
-    // }
-    // public function showP($id)
-    // {
-    //     $listKonten = Konten::with('KomentarKonten')
-    //         ->where('tipe_konten_id', '2')
-    //         ->where('id', $id)
-    //         ->with('LikeKonten')
-    //         ->with('ShareKonten')
-    //         ->orderBy('created_at', 'DESC')
-    //         ->get();
-    //     // dd($listKonten->toArray());
-    //     return view('beranda.podcast', compact('listKonten', 'id'));
-    // }
+    public function indexPodcast()
+    {
+        $listKonten = Konten::with('KomentarKonten')
+            ->where('tipe_konten_id', '2')
+            ->with('LikeKonten')
+            ->with('ShareKonten')
+            ->orderBy('created_at', 'DESC')
+            ->get();
+        // dd($listKonten->toArray());
+        return view('beranda.list_podcast', compact('listKonten'));
+    }
+    public function showPodcast($id)
+    {
+        $listKonten = Konten::with('KomentarKonten')
+            ->where('tipe_konten_id', '2')
+            ->where('id', $id)
+            ->with('LikeKonten')
+            ->with('ShareKonten')
+            ->orderBy('created_at', 'DESC')
+            ->get();
+        // dd($listKonten->toArray());
+        return view('beranda.podcast', compact('listKonten', 'id'));
+    }
     // //Tontonan
-    // public function indexT()
-    // {
-    //     $listKonten = Konten::with('KomentarKonten')
-    //         ->where('tipe_konten_id', '3')
-    //         ->with('LikeKonten')
-    //         ->with('ShareKonten')
-    //         ->orderBy('created_at', 'DESC')
-    //         ->get();
-    //     // dd($listKonten->toArray());
-    //     return view('admin/beranda.list_konten', compact('listKonten'));
-    // }
-    // public function showT($id)
-    // {
-    //     $listKonten = Konten::with('KomentarKonten')
-    //         ->where('tipe_konten_id', '3')
-    //         ->where('id', $id)
-    //         ->with('LikeKonten')
-    //         ->with('ShareKonten')
-    //         ->orderBy('created_at', 'DESC')
-    //         ->get();
-    //     // dd($listKonten->toArray());
-    //     return view('beranda.tontonan', compact('listKonten', 'id'));
-    // }
+    public function indexTontonan()
+    {
+        $listKonten = Konten::with('KomentarKonten')
+            ->where('tipe_konten_id', '3')
+            ->with('LikeKonten')
+            ->with('ShareKonten')
+            ->orderBy('created_at', 'DESC')
+            ->get();
+        // dd($listKonten->toArray());
+        return view('admin/beranda.list_konten', compact('listKonten'));
+    }
+    public function showTontonan($id)
+    {
+        $listKonten = Konten::with('KomentarKonten')
+            ->where('tipe_konten_id', '3')
+            ->where('id', $id)
+            ->with('LikeKonten')
+            ->with('ShareKonten')
+            ->orderBy('created_at', 'DESC')
+            ->get();
+        // dd($listKonten->toArray());
+        return view('beranda.tontonan', compact('listKonten', 'id'));
+    }
 
     /**
      * Show the form for creating a new resource.

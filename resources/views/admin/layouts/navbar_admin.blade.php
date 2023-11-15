e24d963a87b431d5aa1c5355cacd11529404111f
PaulCak28 <paulussigalingging2803@gmail.com>
2023-11-15 22:35:26 +0700

    sikit lagi

--- resources/views/admin/layouts/navbar_admin.blade.php
+++ resources/views/admin/layouts/navbar_admin.blade.php
@@ -390,10 +390,10 @@
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-content">
-                                  <a class="dropdown-item" href="#">
+                                  {{-- <a class="dropdown-item" href="#">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
-                                </a>
+                                </a> --}}
                                 <div class="dropdown-divider dropdown-menu dropdown-menu-right shadow animated--grow-in"></div>
                                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
